7f1de13ee45c7c9c81165bfb5a6e97e5525525ac
Gayeong Lee <gayyeong@naver.com>
2023-08-16 23:53:32 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class Todo {
+  bool isDone = false;
+  String title;
+
+  Todo(this.title, {this.isDone = false});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '할일 관리',
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final _items = <Todo>[];
+  var _todoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('남은 할 일')),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: _todoController,
+                )),
+                ElevatedButton(
+                    onPressed: () => _addTodo(Todo(_todoController.text)),
+                    child: Text('추가'))
+              ],
+            ),
+            StreamBuilder<QuerySnapshot>(
+                stream: // 어떤 컬랙션을 확인할건지(update)
+                    FirebaseFirestore.instance.collection('todo').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    // 데이터가 없으면 들어올때까지 로딩해라
+                    return CircularProgressIndicator();
+                  }
+                  final documents = snapshot.data!.docs; // 있다면 documents에 저장
+                  return Expanded(
+                      child: ListView(
+                    children:
+                        documents.map((doc) => _buildItemWidget(doc)).toList(),
+                  ));
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _addTodo(Todo todo) {
+    FirebaseFirestore.instance
+        .collection('todo')
+        .add({'title': todo.title, 'isDone': todo.isDone});
+    _todoController.text = '';
+  }
+
+  void _deleteTodo(DocumentSnapshot Doc) {
+    FirebaseFirestore.instance.collection('todo').doc(Doc.id).delete();
+  }
+
+  void _toggleTodo(DocumentSnapshot Doc) {
+    FirebaseFirestore.instance
+        .collection('todo')
+        .doc(Doc.id)
+        .update({'isDone': !Doc['isDone']});
+  }
+
+  Widget _buildItemWidget(DocumentSnapshot doc) {
+    final todo = Todo(doc['title'], isDone: doc['isDone']);
+    return ListTile(
+      onTap: () => _toggleTodo(doc),
+      title: Text(
+        todo.title,
+        style: todo.isDone
+            ? TextStyle(
+                decoration: TextDecoration.lineThrough,
+                fontStyle: FontStyle.italic)
+            : null,
+      ),
+      trailing: IconButton(
+          onPressed: () => _deleteTodo(doc), icon: Icon(Icons.delete_forever)),
+    );
+  }
+}
